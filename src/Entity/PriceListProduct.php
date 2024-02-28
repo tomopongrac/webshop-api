fc35e676fc01a3eff1afbf07a73b33c9055390b5
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2024-02-28 18:15:26 +0100

    Create PriceListProduct entity

---
+++ src/Entity/PriceListProduct.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomoPongrac\WebshopApiBundle\Entity;
+
+class PriceListProduct
+{
+    use TimestampableTrait;
+
+    private ?int $id = null;
+
+    private int $price;
+
+    private PriceList $priceList;
+
+    private Product $product;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getPriceList(): PriceList
+    {
+        return $this->priceList;
+    }
+
+    public function setPriceList(PriceList $priceList): static
+    {
+        $this->priceList = $priceList;
+
+        return $this;
+    }
+
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(Product $product): static
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+}
